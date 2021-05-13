84db1469d53479d2681d5530a964a6e3b3fc9a80
Your Name <you@example.com>
2021-05-13 16:43:41 +0430

    add sleep duration

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dotenv import dotenv_values
 from telethon import TelegramClient, events, sync
 import argparse
+from time import sleep
 
 
 parser = argparse.ArgumentParser()
@@ -14,6 +15,13 @@ parser.add_argument(
     help="target chat line or id",
     default=None,
 )
+parser.add_argument(
+    "--sleep-time",
+    help="sleep duration between messages, to avoid rate limit, recommended more than 1 seconds",
+    default=None,
+    required=False,
+)
+
 args = parser.parse_args()
 
 config = dotenv_values(".env")
