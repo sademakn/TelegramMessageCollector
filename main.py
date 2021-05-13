bf62963e80005c256e9b9420beb5d6551ba8e171
Your Name <you@example.com>
2021-05-13 16:34:54 +0430

    add argparse

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from dotenv import dotenv_values
 from telethon import TelegramClient, events, sync
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--source-chat",
+    help="source chat line or id",
+    default=None,
+)
+parser.add_argument(
+    "--target-chat",
+    help="target chat line or id",
+    default=None,
+)
+args = parser.parse_args()
 
 config = dotenv_values(".env")
 
