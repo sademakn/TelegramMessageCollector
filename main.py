88d544b6278ed4861ad5e1a74262a3792946c06c
Your Name <you@example.com>
2021-05-13 16:16:05 +0430

    add telethon client creator

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from dotenv import dotenv_values
+from telethon import TelegramClient, events, sync
 
 config = dict(dotenv_values(".env"))
+
+
+client = TelegramClient("session_name", config.api_id, config.api_hash)
+client.start()
